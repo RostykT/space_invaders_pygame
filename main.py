96e9fa20005d2e8696717151cb0fadddbca2b4c2
Rostyk <rostishka1@gmail.com>
2021-09-22 22:23:34 +0300

    move on the x-axis

--- main.py
+++ main.py
@@ -19,22 +19,34 @@ playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 500
 
-# player on screen
+playerX_change = 0
 
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
 # game loop
 running = True
 while running:
+    # rgb background
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # rgb background
-    screen.fill((0, 0, 0))
-    player()
+    # move
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = +0.3
+        # check  wherther we release the button
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    player(playerX, playerY)
 
     pygame.display.update()
